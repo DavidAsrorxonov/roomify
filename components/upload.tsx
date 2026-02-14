bc9a83e7bce202ca0f1772f0457b4ea708d3a143
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-02-14 16:06:54 +0900

    Refactor Upload component to manage file state and integrate context for authentication

--- components/upload.tsx
+++ components/upload.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useOutletContext } from "react-router";
+import type { AuthContext } from "types/auth-state";
 
 const Upload = () => {
-  return <div>Upload</div>;
+  const [file, setFile] = React.useState<File | null>(null);
+  const [isDragging, setIsDragging] = React.useState(false);
+  const [progress, setProgress] = React.useState(0);
+
+  const { isSignedIn } = useOutletContext<AuthContext>();
+
+  return <div className="upload"></div>;
 };
 
 export default Upload;
